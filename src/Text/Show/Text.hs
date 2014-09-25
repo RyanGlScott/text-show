faea9be317f1850e4bd1da20d05fe5f2a4297dd2
RyanGlScott <ryan.gl.scott@ku.edu>
2014-09-25 15:18:30 -0500

    Use text-format

--- src/Text/Show/Text.hs
+++ src/Text/Show/Text.hs
@@ -34,17 +34,19 @@ import           Data.Ratio
 import qualified Data.Set as S
 import           Data.Set (Set)
 import           Data.Text (Text)
+import           Data.Text.Buildable (build)
 import           Data.Text.IO
 import           Data.Text.Lazy (toStrict)
 import           Data.Text.Lazy.Builder (Builder, toLazyText)
-import           Data.Text.Lazy.Builder.Int
 import           Data.Text.Lazy.Builder.RealFloat
 import           Data.Word
+
+import           Foreign.Ptr
 
 import           Prelude hiding (Show(..), print, putStrLn)
 
 import           Text.Show.Text.Char
-import           Text.Show.Text.Integer
+-- import           Text.Show.Text.Integer
 import           Text.Show.Text.Util
 
 -- | Conversion of values to 'Text'.
@@ -103,7 +105,7 @@ unwordsB [b]          = b
 unwordsB []           = mempty
 
 instance Show Builder where
-    showb b = b
+    showb = id
 
 instance Show () where
     showb () = "()"
@@ -111,7 +113,10 @@ instance Show () where
 instance Show Char where
     showb '\'' = "'\\''"
     showb c    = s '\'' <> showbLitChar c <> s '\''
-    showbList cs = s '"' <> showbLitString cs <> s '"'
+    {-# INLINE showb #-}
+--     showbList cs = s '"' <> showbLitString cs <> s '"'
+    showbList = build
+    {-# INLINE showbList #-}
 
 instance Show Bool where
     showb True  = "True"
@@ -119,64 +124,101 @@ instance Show Bool where
 
 instance Show a => Show [a] where
     showb = showbList
+    {-# INLINE showb #-}
 
 instance Show Int where
-    showb = decimal
-    showbPrec k i = showbParen (i < 0 && k > 0) $ decimal i
+--     showb = decimal
+    showb = build
+    {-# INLINE showb #-}
+    showbPrec k i = showbParen (i < 0 && k > 0) $ build i
+    {-# INLINE showbPrec #-}
 
 instance Show Int8 where
-    showb = decimal
-    showbPrec k i = showbParen (i < 0 && k > 0) $ decimal i
+--     showb = decimal
+    showb = build
+    {-# INLINE showb #-}
+    showbPrec k i = showbParen (i < 0 && k > 0) $ build i
+    {-# INLINE showbPrec #-}
 
 instance Show Int16 where
-    showb = decimal
-    showbPrec k i = showbParen (i < 0 && k > 0) $ decimal i
+--     showb = decimal
+    showb = build
+    {-# INLINE showb #-}
+    showbPrec k i = showbParen (i < 0 && k > 0) $ build i
+    {-# INLINE showbPrec #-}
 
 instance Show Int32 where
-    showb = decimal
-    showbPrec k i = showbParen (i < 0 && k > 0) $ decimal i
+--     showb = decimal
+    showb = build
+    {-# INLINE showb #-}
+    showbPrec k i = showbParen (i < 0 && k > 0) $ build i
+    {-# INLINE showbPrec #-}
 
 instance Show Int64 where
-    showb = decimal
-    showbPrec k i = showbParen (i < 0 && k > 0) $ decimal i
-
-instance Show Word where
-    showb = decimal
-
-instance Show Word8 where
-    showb = decimal
-
-instance Show Word16 where
-    showb = decimal
-
-instance Show Word32 where
-    showb = decimal
-
-instance Show Word64 where
-    showb = decimal
+--     showb = decimal
+    showb = build
+    {-# INLINE showb #-}
+    showbPrec k i = showbParen (i < 0 && k > 0) $ build i
+    {-# INLINE showbPrec #-}
 
 instance Show Integer where
-    showb = showbInteger
-    showbPrec k i = showbParen (i < 0 && k > 0) $ showbInteger i
+--     showb = showbInteger
+    showb = build
+    {-# INLINE showb #-}
+    showbPrec k i = showbParen (i < 0 && k > 0) $ build i
+    {-# INLINE showbPrec #-}
+
+instance Show Word where
+--     showb = decimal
+    showb = build
+    {-# INLINE showb #-}
+
+instance Show Word8 where
+--     showb = decimal
+    showb = build
+    {-# INLINE showb #-}
+
+instance Show Word16 where
+--     showb = decimal
+    showb = build
+    {-# INLINE showb #-}
+
+instance Show Word32 where
+--     showb = decimal
+    showb = build
+    {-# INLINE showb #-}
+
+instance Show Word64 where
+--     showb = decimal
+    showb = build
+    {-# INLINE showb #-}
 
 instance Show Float where
     showb = realFloat
+    {-# INLINE showb #-}
     showbPrec k f = showbParen (f < 0 && k > 0) $ realFloat f
+    {-# INLINE showbPrec #-}
 
 instance Show Double where
     showb = realFloat
+    {-# INLINE showb #-}
     showbPrec k f = showbParen (f < 0 && k > 0) $ realFloat f
+    {-# INLINE showbPrec #-}
 
 instance (Show a, Integral a) => Show (Ratio a) where
+    {-# SPECIALIZE instance Show Rational #-}
     showbPrec k q = showbParen (k > 7) $ showbPrec 8 (numerator q) <>
                     s '%' <> showb (denominator q)
 
 instance (Show a, RealFloat a) => Show (Complex a) where
+    {-# SPECIALIZE instance Show (Complex Float) #-}
+    {-# SPECIALIZE instance Show (Complex Double) #-}
     showbPrec k (a :+ b) = showbParen (k > 6) $ showbPrec 7 a <> " :+ " <> showbPrec 7 b
 
 instance Show a => Show (Maybe a) where
     showbPrec _ Nothing  = "Nothing"
     showbPrec k (Just a) = showbParen (k > 10) $ "Just " <> showbPrec 11 a
+    {-# INLINE showbPrec #-}
 
 instance (Show a, Show b) => Show (Either a b) where
     showbPrec k (Left a)  = showbParen (k > 10) $ "Left " <> showbPrec 11 a
@@ -192,6 +234,7 @@ instance (Show a, Show b) => Show (a, b) where
       s '(' <> showb a <>
       s ',' <> showb b <>
       s ')'
+    {-# INLINE showb #-}
 
 instance (Show a, Show b, Show c) => Show (a, b, c) where
     showb (a, b, c) =
@@ -199,6 +242,7 @@ instance (Show a, Show b, Show c) => Show (a, b, c) where
       s ',' <> showb b <>
       s ',' <> showb c <>
       s ')'
+    {-# INLINE showb #-}
 
 instance (Show a, Show b, Show c, Show d) => Show (a, b, c, d) where
     showb (a, b, c, d) =
@@ -207,6 +251,7 @@ instance (Show a, Show b, Show c, Show d) => Show (a, b, c, d) where
       s ',' <> showb c <>
       s ',' <> showb d <>
       s ')'
+    {-# INLINE showb #-}
 
 instance (Show a, Show b, Show c, Show d, Show e) => Show (a, b, c, d, e) where
     showb (a, b, c, d, e) =
@@ -216,6 +261,7 @@ instance (Show a, Show b, Show c, Show d, Show e) => Show (a, b, c, d, e) where
       s ',' <> showb d <>
       s ',' <> showb e <>
       s ')'
+    {-# INLINE showb #-}
 
 instance (Show a, Show b, Show c, Show d, Show e, Show f) => Show (a, b, c, d, e, f) where
     showb (a, b, c, d, e, f) =
@@ -226,6 +272,7 @@ instance (Show a, Show b, Show c, Show d, Show e, Show f) => Show (a, b, c, d, e
       s ',' <> showb e <>
       s ',' <> showb f <>
       s ')'
+    {-# INLINE showb #-}
 
 instance (Show a, Show b, Show c, Show d, Show e, Show f, Show g) =>
   Show (a, b, c, d, e, f, g) where
@@ -238,6 +285,7 @@ instance (Show a, Show b, Show c, Show d, Show e, Show f, Show g) =>
       s ',' <> showb f <>
       s ',' <> showb g <>
       s ')'
+    {-# INLINE showb #-}
 
 instance (Show a, Show b, Show c, Show d, Show e, Show f, Show g, Show h) =>
   Show (a, b, c, d, e, f, g, h) where
@@ -251,6 +299,7 @@ instance (Show a, Show b, Show c, Show d, Show e, Show f, Show g, Show h) =>
       s ',' <> showb g <>
       s ',' <> showb h <>
       s ')'
+    {-# INLINE showb #-}
 
 instance (Show a, Show b, Show c, Show d, Show e, Show f, Show g, Show h, Show i) =>
   Show (a, b, c, d, e, f, g, h, i) where
@@ -265,6 +314,7 @@ instance (Show a, Show b, Show c, Show d, Show e, Show f, Show g, Show h, Show i
       s ',' <> showb h <>
       s ',' <> showb i <>
       s ')'
+    {-# INLINE showb #-}
 
 instance (Show a, Show b, Show c, Show d, Show e, Show f, Show g, Show h, Show i, Show j) =>
   Show (a, b, c, d, e, f, g, h, i, j) where
@@ -280,6 +330,7 @@ instance (Show a, Show b, Show c, Show d, Show e, Show f, Show g, Show h, Show i
       s ',' <> showb i <>
       s ',' <> showb j <>
       s ')'
+    {-# INLINE showb #-}
 
 instance (Show a, Show b, Show c, Show d, Show e, Show f,
           Show g, Show h, Show i, Show j, Show k) =>
@@ -297,6 +348,7 @@ instance (Show a, Show b, Show c, Show d, Show e, Show f,
       s ',' <> showb j <>
       s ',' <> showb k <>
       s ')'
+    {-# INLINE showb #-}
 
 instance (Show a, Show b, Show c, Show d, Show e, Show f,
           Show g, Show h, Show i, Show j, Show k, Show l) =>
@@ -315,6 +367,7 @@ instance (Show a, Show b, Show c, Show d, Show e, Show f,
       s ',' <> showb k <>
       s ',' <> showb l <>
       s ')'
+    {-# INLINE showb #-}
 
 instance (Show a, Show b, Show c, Show d, Show e, Show f, Show g,
           Show h, Show i, Show j, Show k, Show l, Show m) =>
@@ -334,6 +387,7 @@ instance (Show a, Show b, Show c, Show d, Show e, Show f, Show g,
       s ',' <> showb l <>
       s ',' <> showb m <>
       s ')'
+    {-# INLINE showb #-}
 
 instance (Show a, Show b, Show c, Show d, Show e, Show f, Show g,
           Show h, Show i, Show j, Show k, Show l, Show m, Show n) =>
@@ -354,6 +408,7 @@ instance (Show a, Show b, Show c, Show d, Show e, Show f, Show g,
       s ',' <> showb m <>
       s ',' <> showb n <>
       s ')'
+    {-# INLINE showb #-}
 
 instance (Show a, Show b, Show c, Show d, Show e, Show f, Show g, Show h,
           Show i, Show j, Show k, Show l, Show m, Show n, Show o) =>
@@ -375,15 +430,31 @@ instance (Show a, Show b, Show c, Show d, Show e, Show f, Show g, Show h,
       s ',' <> showb n <>
       s ',' <> showb o <>
       s ')'
+    {-# INLINE showb #-}
 
 instance (Show i, Show e, Ix i) => Show (Array i e) where
     showbPrec k a = showbParen (k > 10) $ "array " <>
                     showb (bounds a) <> s ' ' <> showb (assocs a)
+    {-# INLINE showb #-}
 
 instance (Show k, Show v) => Show (Map k v) where
     showbPrec k m = showbParen (k > 10) $ "fromList " <>
                     showb (M.toList m)
+    {-# INLINE showb #-}
 
 instance Show e => Show (Set e) where
     showbPrec k set = showbParen (k > 10) $ "fromList " <>
                       showb (S.toList set)
+    {-# INLINE showb #-}
+
+instance Show IntPtr where
+    showb = build
+    {-# INLINE showb #-}
+
+instance Show WordPtr where
+    showb = build
+    {-# INLINE showb #-}
+
+instance Show (Ptr a) where
+    showb = build
+    {-# INLINE showb #-}
