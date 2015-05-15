93ab077dcfbb9fd2a7893b6a47e2f469fe4820a2
RyanGlScott <ryan.gl.scott@ku.edu>
2015-05-15 09:18:54 -0500

    Fix test suite with earlier QuickChecks

--- tests/Instances/Numeric/Natural.hs
+++ tests/Instances/Numeric/Natural.hs
@@ -13,11 +13,23 @@ Portability: GHC
 -}
 module Instances.Numeric.Natural () where
 
+-- Copied from @QuickCheck@
+
 #if !(MIN_VERSION_QuickCheck(2,8,0)) || !(MIN_VERSION_base(4,8,0))
 import Numeric.Natural (Natural)
-import Test.QuickCheck (Arbitrary(..), arbitrarySizedNatural, shrinkIntegral)
+import Test.QuickCheck (Arbitrary(..), Gen,
+                        choose, shrinkIntegral, sized, suchThat)
 
 instance Arbitrary Natural where
     arbitrary = arbitrarySizedNatural
     shrink    = shrinkIntegral
+
+-- | Generates a natural number. The number's maximum value depends on
+-- the size parameter.
+arbitrarySizedNatural :: Integral a => Gen a
+arbitrarySizedNatural = sized $ \n ->
+    inBounds fromInteger (choose (0, toInteger n))
+
+inBounds :: Integral a => (Integer -> a) -> Gen Integer -> Gen a
+inBounds fi g = fmap fi (g `suchThat` (\x -> toInteger (fi x) == x))
 #endif
