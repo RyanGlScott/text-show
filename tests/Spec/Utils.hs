f97aea930920c7b1e66cf0ba8812b5f5886ce539
Ryan Scott <ryan.gl.scott@gmail.com>
2016-11-22 10:48:19 -0500

    Axe some TODOs

--- tests/Spec/Utils.hs
+++ tests/Spec/Utils.hs
@@ -44,7 +44,8 @@ import Data.Functor.Classes (Show2, showsPrec2)
 import TextShow (TextShow2(..), showbPrec2)
 #endif
 
--- TODO RGS
+-- | Expect a type's 'Show' instances to coincide for both 'String's and 'Text',
+-- irrespective of precedence.
 matchesTextShowSpec :: forall a. (Arbitrary a, Show a, TextShow a)
                     => Proxy a -> Spec
 matchesTextShowSpec _ = prop "TextShow instance" (prop_matchesTextShow :: Int -> a -> Bool)
@@ -54,7 +55,8 @@ matchesTextShowSpec _ = prop "TextShow instance" (prop_matchesTextShow :: Int ->
 prop_matchesTextShow :: (Show a, TextShow a) => Int -> a -> Bool
 prop_matchesTextShow p x = fromString (showsPrec p x "") == showbPrec p x
 
--- TODO RGS
+-- | Expect a type's 'Show1' instances to coincide for both 'String's and 'Text',
+-- irrespective of precedence.
 matchesTextShow1Spec :: forall f a.
                         (Arbitrary (f a), Show1 f, Show a, Show (f a), TextShow1 f, TextShow a)
                      => Proxy (f a) -> Spec
@@ -66,7 +68,8 @@ prop_matchesTextShow1 :: (Show1 f, Show a, TextShow1 f, TextShow a) => Int -> f 
 prop_matchesTextShow1 p x = fromString (showsPrec1 p x "") == showbPrec1 p x
 
 #if defined(NEW_FUNCTOR_CLASSES)
--- TODO RGS
+-- | Expect a type's 'Show2' instances to coincide for both 'String's and 'Text',
+-- irrespective of precedence.
 matchesTextShow2Spec :: forall f a b.
                         (Arbitrary (f a b), Show2 f, Show a, Show b, Show (f a b),
                          TextShow2 f, TextShow a, TextShow b)
@@ -80,7 +83,8 @@ prop_matchesTextShow2 :: (Show2 f, Show a, Show b, TextShow2 f, TextShow a, Text
 prop_matchesTextShow2 p x = fromString (showsPrec2 p x "") == showbPrec2 p x
 #endif
 
--- TODO RGS
+-- | Expect a type's 'TextShow' instance to coincide with the output produced
+-- by the equivalent 'Generic' functions.
 genericTextShowSpec :: forall a. (Arbitrary a, Show a, TextShow a,
                                   Generic a, GTextShowB Zero (Rep a))
                     => Proxy a -> Spec
@@ -92,7 +96,8 @@ prop_genericTextShow :: (TextShow a, Generic a, GTextShowB Zero (Rep a))
                      => Int -> a -> Bool
 prop_genericTextShow p x = showbPrec p x == genericShowbPrec p x
 
--- TODO RGS
+-- | Expect a type's 'TextShow1' instance to coincide with the output produced
+-- by the equivalent 'Generic1' functions.
 genericTextShow1Spec :: forall f a. (Arbitrary (f a), Show (f a), TextShow1 f,
                                      Generic1 f, GTextShowB One (Rep1 f), TextShow a)
                      => Proxy (f a) -> Spec
