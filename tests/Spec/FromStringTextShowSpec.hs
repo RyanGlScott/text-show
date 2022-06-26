f3dc4113aa2fe2c824471181d83b2969733eedad
Ryan Scott <ryan.gl.scott@gmail.com>
2022-06-26 17:11:26 -0400

    Scale back instances in TextShow.FromStringTextShow
    This is needed for forward compatibility with
    haskell/core-libraries-committee#10, which proposes to add quantified `Show`
    superclasses to `Show1` and `Show2`. Unfortunately, several of the instances in
    `TextShow.FromStringTextShow` do not mesh well with these new superclasses, so
    several of the instances have to either be removed or have their instance
    contexts changed as a consequence. It's a bit heavy-handed, but I'm not sure
    there exists a better approach. See the `CHANGELOG` for details.
    
    This can be seen as a scaled back version of #55 that only changes instances.
    In particular, it does _not_ add quantified `TextShow` superclasses to
    `TextShow1` or `TextShow2`. See #56, which discusses that point separately.

--- tests/Spec/FromStringTextShowSpec.hs
+++ tests/Spec/FromStringTextShowSpec.hs
@@ -14,12 +14,12 @@ module Spec.FromStringTextShowSpec (main, spec) where
 
 import Data.Proxy.Compat (Proxy(..))
 import Instances.FromStringTextShow ()
-import Spec.Utils (matchesTextShowSpec, matchesTextShow1Spec)
+import Spec.Utils (matchesTextShowSpec)
 import Test.Hspec (Spec, describe, hspec, parallel)
 import TextShow (FromStringShow(..), FromTextShow(..))
 
 #if defined(NEW_FUNCTOR_CLASSES)
-import Spec.Utils (matchesTextShow2Spec)
+import Spec.Utils (matchesTextShow1Spec, matchesTextShow2Spec)
 import TextShow (FromStringShow1(..), FromStringShow2(..),
                  FromTextShow1(..), FromTextShow2(..))
 #endif
@@ -33,43 +33,33 @@ spec = parallel $ do
         let p :: Proxy (FromStringShow Int)
             p = Proxy
         matchesTextShowSpec  p
-        matchesTextShow1Spec p
     describe "FromStringShow String" $ do
         let p :: Proxy (FromStringShow String)
             p = Proxy
         matchesTextShowSpec  p
-        matchesTextShow1Spec p
     describe "FromTextShow Int" $ do
         let p :: Proxy (FromTextShow Int)
             p = Proxy
         matchesTextShowSpec  p
-        matchesTextShow1Spec p
     describe "FromTextShow String" $ do
         let p :: Proxy (FromTextShow String)
             p = Proxy
         matchesTextShowSpec  p
-        matchesTextShow1Spec p
 #if defined(NEW_FUNCTOR_CLASSES)
     describe "FromStringShow1 Maybe Int" $ do
         let p :: Proxy (FromStringShow1 Maybe Int)
             p = Proxy
-        matchesTextShowSpec  p
         matchesTextShow1Spec p
     describe "FromTextShow1 Maybe Int" $ do
         let p :: Proxy (FromTextShow1 Maybe Int)
             p = Proxy
-        matchesTextShowSpec  p
         matchesTextShow1Spec p
     describe "FromStringShow2 Either Char Int" $ do
         let p :: Proxy (FromStringShow2 Either Char Int)
             p = Proxy
-        matchesTextShowSpec  p
-        matchesTextShow1Spec p
         matchesTextShow2Spec p
     describe "FromTextShow2 Either Char Int" $ do
         let p :: Proxy (FromTextShow2 Either Char Int)
             p = Proxy
-        matchesTextShowSpec  p
-        matchesTextShow1Spec p
         matchesTextShow2Spec p
 #endif
